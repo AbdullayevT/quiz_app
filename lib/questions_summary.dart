9e8c84f2926f8c256d494275a878230042e7578a
ATapdiq <abdullayevtapdiq2@gmail.com>
2025-02-09 18:39:48 +0400

    first commit

---
+++ lib/questions_summary.dart
@@ -0,0 +1,24 @@
+import 'package:first_app/summary_item.dart';
+import 'package:flutter/material.dart%20';
+
+class QuestionsSummary extends StatelessWidget {
+  const QuestionsSummary(this.summaryData, {super.key});
+
+  final List<Map<String, Object>> summaryData;
+
+  @override
+  Widget build(context) {
+    return SizedBox(
+      height: 400,
+      child: SingleChildScrollView(
+        child: Column(
+          children: summaryData.map(
+            (data) {
+              return SummaryItem(data);
+            },
+          ).toList(),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/result_screen.dart
@@ -0,0 +1,89 @@
+import 'package:first_app/data/questions.dart';
+import 'package:first_app/questions_summary.dart';
+import 'package:flutter/material.dart%20';
+import 'package:google_fonts/google_fonts.dart';
+
+class ResultScreen extends StatelessWidget {
+  const ResultScreen({
+    super.key,
+    required this.chosenAnswers,
+    required this.onRestart,
+    required this.onGoToHome,
+  });
+
+  final void Function() onRestart;
+  final void Function() onGoToHome;
+  final List<String> chosenAnswers;
+
+  List<Map<String, Object>> getSummary() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summary.add({
+        'question_index': i,
+        'question': questions[i].text,
+        'correct_answer': questions[i].answers[0],
+        'user_answer': chosenAnswers[i],
+      });
+    }
+
+    return summary;
+  }
+
+  @override
+  Widget build(context) {
+    final summaryData = getSummary();
+    final numTotalQuestions = questions.length;
+    final numCorrectQuestions = summaryData.where((data) {
+      return data['correct_answer'] == data['user_answer'];
+    }).length;
+
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'You answered $numCorrectQuestions out of $numTotalQuestions questions correctly!',
+              style: GoogleFonts.lato(
+                color: const Color.fromARGB(255, 230, 200, 253),
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            QuestionsSummary(summaryData),
+            const SizedBox(
+              height: 30,
+            ),
+            Row(
+              children: [
+                TextButton.icon(
+                  onPressed: onGoToHome,
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.white,
+                  ),
+                  icon: const Icon(Icons.home),
+                  label: const Text('Go to Home page'),
+                ),
+                TextButton.icon(
+                  onPressed: onRestart,
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.white,
+                  ),
+                  icon: const Icon(Icons.refresh),
+                  label: const Text('Restart Quiz!'),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
